e2f57f56253d7baf5b3a02be4b28c733cde7ee61
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-09-13 18:39:41 +0600

    firebase google login

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBHbB-sJzMAawc7B0cwocJck0KN5YQ2f48",
+    authDomain: "burj-al-arab-restaurant.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-restaurant.firebaseio.com",
+    projectId: "burj-al-arab-restaurant",
+    storageBucket: "burj-al-arab-restaurant.appspot.com",
+    messagingSenderId: "556459792815",
+    appId: "1:556459792815:web:e2f55ec25865835b2ab75a"
+  };
+
+export default firebaseConfig;
