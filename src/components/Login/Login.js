8df5c781aa96e63526435712bed1ac9d235fa913
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-10-01 13:18:35 +0600

    firebase JWT tokenon client site

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,54 +2,64 @@ import React, { useContext } from 'react';
 import * as firebase from "firebase/app";
 import "firebase/auth";
 import firebaseConfig from './firebase.config';
-import {UserContext} from '../../App';
+import { UserContext } from '../../App';
 import { useHistory, useLocation } from 'react-router-dom';
 
 
 const Login = () => {
-  
-  const [ loggedInUser, setLoggedInUser] = useContext(UserContext);
+
+  const [loggedInUser, setLoggedInUser] = useContext(UserContext);
   let history = useHistory();
   let location = useLocation();
 
   let { from } = location.state || { from: { pathname: "/" } };
 
-  if(firebase.apps.length === 0){
+  if (firebase.apps.length === 0) {
     firebase.initializeApp(firebaseConfig);
   }
 
-  const handleGoogleSignIn =()=> {
+  const handleGoogleSignIn = () => {
     const provider = new firebase.auth.GoogleAuthProvider();
 
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var {displayName, email} = result.user;
-        const signedInUser = {name:displayName, email}
-        console.log(signedInUser);
-        setLoggedInUser(signedInUser)
-        history.replace(from);
-        // ...
-      }).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-      });
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var { displayName, email } = result.user;
+      const signedInUser = { name: displayName, email }
+      console.log(signedInUser);
+      setLoggedInUser(signedInUser)
+      storeAuthToken();
+      history.replace(from);
+      // ...
+    }).catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
 
   }
 
-    return (
-        <div>
-            <h1>This is Login</h1>
-            <button onClick={handleGoogleSignIn}> Sign in With Google </button>
-        </div>
-    );
+  const storeAuthToken = () => {
+    firebase.auth().currentUser.getIdToken(/* forceRefresh */ true)
+    .then(function (idToken) {
+      sessionStorage.setItem("token", idToken)
+    }).catch(function (error) {
+      // Handle error
+    });
+  }
+
+  return (
+    <div>
+      <h1>This is Login</h1>
+      <button onClick={handleGoogleSignIn}> Sign in With Google </button>
+    </div>
+  );
 };
 
 export default Login;
